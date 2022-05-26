746c80ea07d8362a4385e5b9ea99019839763fcc
SantaRenata <renatasanta2002@gmail.com>
2022-05-26 09:29:17 +0200

    Feladat megoldva

--- src/calc.php
+++ src/calc.php
@@ -7,3 +7,28 @@ Date: 2022-05-26
 Github: https://github.com/SantaRenata/
 Licenc: GNU GPL
 -->
+
+<?php
+echo file_get_contents("templates/head.html");
+
+$page = file_get_contents("templates/calc.html");
+
+function calcBody($tomeg,$magassag){
+    $testindex = $tomeg/pow($magassag,2);
+    
+    return $testindex;
+}
+if (!empty($_GET['tomeg']) and !empty($_GET['magassag'])){
+    $tomeg = $_GET['tomeg'];
+    $magassag = $_GET['magassag'];
+    $testindex = calcBody($tomeg,$magassag);
+}else {
+    $testindex = "Hiba! Helytelen adatok.";
+}
+
+$page = str_replace("{{test}}",$testindex,$page);
+
+echo $page;
+
+echo file_get_contents("templates/foot.html");
+?>
--- src/index.php
+++ src/index.php
@@ -7,3 +7,9 @@ Date: 2022-05-26
 Github: https://github.com/SantaRenata/
 Licenc: GNU GPL
 -->
+
+<?php
+echo file_get_contents("templates/head.html");
+echo file_get_contents("templates/home.html");
+echo file_get_contents("templates/foot.html");
+?>
