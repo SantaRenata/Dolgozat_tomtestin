2e48513ae9ebaf0a06dbcd93c96d2ecb4d21de0d
SantaRenata <renatasanta2002@gmail.com>
2022-05-26 08:41:46 +0200

    Forráskód megírva

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<!--
+File: index.php
+Author: Sánta Renáta Diána
+Copyright: 2022, Sánta Renáta Diána
+Group: Szoft I N
+Date: 2022-05-26
+Github: https://github.com/SantaRenata/
+Licenc: GNU GPL
+-->
